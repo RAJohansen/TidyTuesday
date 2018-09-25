300d3a0e88bcb9efdb8cdbbc7c07d57d14777ecf
Richard Johansen <richard.johansen@uc.edu>
2018-09-25 12:18:47 -0400

    Added Code

--- TidyTuesday#26.R
+++ TidyTuesday#26.R
@@ -1,28 +1,24 @@
 ### Tidy Tuesday Week 26 (September 25 2018)
 library(raster)                               
 library(tidyverse)
-library(maps)
+library(tmap)
+library(sp)
 
 df_invasion <- read.csv("C:/R_Packages/TidyTuesday/Data/TT_26/table_1.csv")
 df_cost <- read.csv("C:/R_Packages/TidyTuesday/Data/TT_26/table_2.csv")
 df <- merge(df_cost,df_invasion,by="country")
+colnames(df) <- c("name","Cost", "Rank_Cost", "Rank_Threat", "Threat")
 
-colnames(df) <- c("Country","Cost", "Rank_Cost", "Rank_Threat", "Threat")
-df$region <- df$Country
+World_Pest <- sp::merge(World, df, by ="name", duplicateGeoms = TRUE)
 
-world <- map_data("world")
+tm_shape(World_Pest) +
+  tm_fill(col = "Threat", n = 10, colorNA = "white") +
+  tm_borders("grey50") +  
+  tm_compass(type = "8star", position = c("left", "top")) +
+  tm_layout(frame = FALSE)
 
-choro <- merge(world, df, sort = FALSE, by = "region")
-choro <- choro[order(choro$order), ]
-
-ggplot(choro, aes(long, lat)) +
-  geom_polygon(aes(group = group, fill = Cost)) +
-  coord_map(projection = "mercator") + 
-  theme_minimal()
-
-
-
-ggplot(choro, aes(long, lat)) +
-  geom_polygon(aes(group = group, fill = Rank_Cost*Rank_Threat)) +
-  coord_map(projection = "mercator") + 
-  theme_minimal()
+tm_shape(World_Pest) +
+  tm_fill(col = "Cost", n = 10, colorNA = "white") +
+  tm_borders("grey50") +  
+  tm_compass(type = "8star", position = c("left", "top")) +
+  tm_layout(frame = FALSE)
