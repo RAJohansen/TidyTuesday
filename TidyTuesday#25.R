b2d19f1db5106f7b7e2dfa85a5c44789a389a2a8
Richard Johansen <richard.johansen@uc.edu>
2018-10-03 08:56:44 -0400

    changed back

--- TidyTuesday#25.R
+++ TidyTuesday#25.R
@@ -61,5 +61,3 @@ ggplot(na.omit(ap)) +
         plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
         legend.title=element_text(size=16), 
         legend.text=element_text(size=12))
-
-edit
