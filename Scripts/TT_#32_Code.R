7fbc57e418328d3595f43d6eb3946b166d119c6d
Richard Johansen <richard.johansen@uc.edu>
2018-11-06 10:14:36 -0500

    Updated to Scatterplot Matrix

--- Scripts/TT_#32_Code.R
+++ Scripts/TT_#32_Code.R
@@ -14,58 +14,48 @@ View(df)
 str(df)
 summary(df)
 
-### Create hierarchical clustering using a Dendrogram --------------------------
+
+#Subset Data for Visualization
 #Select Subset of variables
-# 8 = Project Name
-#12:21 are turbine characteristics
-df1 <- df[,c(8,12:19)]
+#14:16 are turbine characteristics
+#Turbine Characteristics Confidence
+df1 <- df[,c(14:16, 22)]
 
-#Select 100 random observations
-df1 <- df1[sample(1:nrow(df1), 100,
-                 replace=FALSE),]
+#Select 250 random observations
+df1 <- df1[sample(1:nrow(df1), 250,
+                  replace=FALSE),]
 
-#Calculate distance between observations
-d <- dist(df1)
-#Large values = large distance between cars (disimilar)
-#Small value = small distance between cars (similar)
+#Remove all observations with "-9999"
+df1[df1 == -9999] <- NA
+df1 <- df1[complete.cases(df1), ]
 
-#Next we use a hierarchical clustering tool using this distance matrix
-#Default euclidean distance
-c <- hclust(d)
-c
 
-#plot the dendrogram for clusters
-plot(c)
+#To create a basic scatterplot matrix just requires using the pairs function
+pairs(df1[1:3]) #only quantitative variables
 
-#Draw boxes around the clusters in the dendrogram
-rect.hclust(c, k = 3, border = "blue")
-rect.hclust(c, k = 5, border = "green4")
+#Modified scatterplot matricies
+require("RColorBrewer")
+display.brewer.pal(4,"Pastel1") #display colorpalette
 
-### k-means clustering --------------------------------------------------------
-#Subset df1 to remove all non-numeric values
-df2 <- df1[,c(4:9)]
-#Calculate kmeans on df2 for 3 clusters
-km <- kmeans(df2, 3)
-km
+#Put Histograms on the diagonal (from "pairs" Help)
+#Creating new function
+panel.hist  <- function(x,...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(usr[1:2], 0,1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, ...)
+}
 
-#Graph the k-means clustering
-require(cluster)
-clusplot(df2,
-         km$cluster,
-         color = TRUE,
-         #shade = TRUE,
-         lines = 3, # Lines connecting centroids
-         labels = 2) # Labels clusters and cases
+#Re-run pairs using the panel.hist function
+pairs(df1[1:3],
+      panel = panel.smooth,
+      main = "Scatterplot Maxtris for Turbine Characteristics \n Tidy Tuesday Week #32",
+      diag.panel = panel.hist,
+      pch = 16, 
+      col = brewer.pal(4,"Pastel1")[df1$t_img_srce],
+      oma=c(4,4,7,20))
 
-#Calculate kmeans on df2 for 5 clusters
-km1 <- kmeans(df2, 5)
-km
-
-#Graph the k-means clustering
-require(cluster)
-clusplot(df2,
-         km1$cluster,
-         color = TRUE,
-         #shade = TRUE,
-         lines = 3, # Lines connecting centroids
-         labels = 2) # Labels clusters and cases
+legend(locator(), legend = levels(df1$t_img_srce), fill= brewer.pal(4,"Pastel1"))
