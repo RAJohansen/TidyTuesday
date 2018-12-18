7fb01b701572d53173b37d1aa59653746746c7dd
Richard Johansen <richard.johansen@uc.edu>
2018-12-18 09:47:37 -0500

    finished plot

--- Scripts/TT_#38_Code.R
+++ Scripts/TT_#38_Code.R
@@ -6,5 +6,23 @@
 #load tidyverse!
 library(tidyverse)
 
+
 # Load Data
 df <- read.csv("C:/R_Packages/TidyTuesday/Data/TT_38/allCetaceanData.csv")
+
+#Explore structure and data 
+str(df)
+summary(df)
+
+#Remove all rows where birth year is NA
+df1 <- df[!is.na(df$birthYear),]
+
+#Convert birth year from factor to numeric
+df1$birthYear <- as.numeric(as.character(df1$birthYear))
+
+#Plot Number of animals for each gender by species over time
+ggplot(df1) +
+  geom_histogram(aes(birthYear, fill = sex), binwidth =3, position = position_dodge2(preserve = "single")) +
+  facet_wrap(~species) + 
+  scale_y_log10() +
+  theme_classic()
