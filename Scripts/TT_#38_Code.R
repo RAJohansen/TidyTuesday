115edc15844e1bfa9b4997c96cb0e6387b4c5386
Richard Johansen <richard.johansen@uc.edu>
2018-12-18 08:54:20 -0500

    Initial Commit

---
+++ Scripts/TT_#38_Code.R
@@ -0,0 +1,10 @@
+#Tidy Tuesday Week #38
+#By Richard Johansen
+#Twitter: @DataVizJohansen
+#GitHub: RAJohansen
+
+#load tidyverse!
+library(tidyverse)
+
+# Load Data
+df <- read.csv("C:/R_Packages/TidyTuesday/Data/TT_38/allCetaceanData.csv")
