e56f2a57bbd5649b410b552398a9a0fffa03eb7d
Richard Johansen <richard.johansen@uc.edu>
2018-12-18 10:02:24 -0500

    Finalized

--- Scripts/TT_#38_Code.R
+++ Scripts/TT_#38_Code.R
@@ -20,9 +20,18 @@ df1 <- df[!is.na(df$birthYear),]
 #Convert birth year from factor to numeric
 df1$birthYear <- as.numeric(as.character(df1$birthYear))
 
-#Plot Number of animals for each gender by species over time
+#Plot Number of Cetacean Birth Types by species from 1938-2017
+
+jpeg("C:/R_Packages/TidyTuesday/Jpegs/TT_#39.jpeg", width = 20, height = 16, units = 'in', res = 300)
+
 ggplot(df1) +
-  geom_histogram(aes(birthYear, fill = sex), binwidth =3, position = position_dodge2(preserve = "single")) +
+  geom_histogram(aes(birthYear, fill = acquisition), binwidth =5, position = position_dodge2(preserve = "single")) +
   facet_wrap(~species) + 
   scale_y_log10() +
-  theme_classic()
+  theme_classic() +
+  labs(title= "Cetacean Birth Types by species from 1938-2017", x = "Animal's Birth Year", y= "Count") +
+  theme(axis.title.y = element_text(angle=0,vjust = 0.5),
+        plot.title = element_text(hjust = 0.5))
+  
+#ggsave("C:/R_Packages/TidyTuesday/Jpegs/TT_#39.jpeg", units="in", width=20, height=16, dpi=300, compression = 'lzw')
+dev.off()
